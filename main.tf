281dbd510474f7ede791f67929cef75a36e90a6a
samuelakosaonyejekwe <samuelonyejekwe852@gmail.com>
2026-05-01 09:47:29 +0300

    destroy all resources across environments

--- main.tf
+++ main.tf
@@ -10,13 +10,7 @@ terraform {
       version = "~> 3.0"
     }
   }
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
   }
-}
 
 provider "aws" {
   region = "eu-west-2"
@@ -71,6 +65,8 @@ resource "random_id" "rand" {
 }
 
 
-  }
-}
+
+
+
+
 
